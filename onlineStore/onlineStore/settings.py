c8efd5d99e7713d6c4d8d27110b280409b7301e2
Fiona <57137659+fquinn454@users.noreply.github.com>
2023-05-12 09:31:13 +0100

    added gitignore

--- onlineStore/onlineStore/settings.py
+++ onlineStore/onlineStore/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oe)%b$jfm4pce+3&61n!8kga0yhgicsmigu0p54x$kwb*o8ux3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
