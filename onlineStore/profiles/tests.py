cc04f27b8e1b072c7e2a203713c4a2e4720810f8
fquinn454 <fionaqui@gmail.com>
2023-06-15 15:41:48 +0000

    added some tests

--- onlineStore/profiles/tests.py
+++ onlineStore/profiles/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username="testuser1", password="password123", email="my_test_email@gmail.com")
+        self.client = Client()
+
+    def test_profile_string(self):
+        """Profile returns string description"""
+        user1 = User.objects.get(username="testuser1")
+        profile = Profile.objects.get(user = user1)
+        self.assertEqual(str(profile), 'testuser1')
+        self.assertNotEqual(str(profile), 'testuser2')
+
+    def test_user_loggedIn(self):
+        user1 = User.objects.get(username="testuser1")
+        login = self.client.login(username='testuser1', password='password123')
+        login_2 = self.client.login(username='testuser2', password='I_Dont_Know')
+        self.assertTrue(login)
+        self.assertTrue(user1.is_authenticated)
+        self.assertFalse(login_2)
--- onlineStore/store/tests.py
+++ onlineStore/store/tests.py
@@ -1,4 +1,4 @@
 from django.test import TestCase
-from utils import *
+
 
 # Create your tests here.
