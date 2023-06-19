6f9e050e038064d3616eb636fba81f1fafd3c2a8
fquinn454 <fionaqui@gmail.com>
2023-06-19 12:41:30 +0000

    testing profiles.models coverage 45%

--- onlineStore/profiles/tests.py
+++ onlineStore/profiles/tests.py
@@ -14,6 +14,9 @@ class ProfileTestCase(TestCase):
         self.user1.save()
         self.client = Client()
         self.factory = RequestFactory()
+        self.product_1 = Product.objects.create(id=1, title='test_product1', description='A great phone ....', price=159.99, stock=3, rating=4.3, discount=3.5)
+        self.product_3 = Product.objects.create(id=3, title='test_product3', description='A great tablet ....', price=359.99, stock=4, rating=4.25, discount=4.5)
+        self.product_5 = Product.objects.create(id=5, title='test_product5', description='A great laptop ....', price=899.99, stock=4.5, rating=3.96, discount=6.5)
 
 
     def test_profile_created_automatically(self):
@@ -33,18 +36,133 @@ class ProfileTestCase(TestCase):
         # User Login Tests
         self.assertTrue(self.client.login(username='testuser1', password='password123'))
         self.assertTrue(self.user1.is_authenticated)
-
+        self.client.logout()
         # User Login Failed
         login_2 = self.client.login(username='testuser1', password='I_Dont_Know')
         self.assertFalse(login_2)
 
 
     def test_addProductToWishlist(self):
-        request = self.factory.post('/addFavourite', {'wishlist': []})
+        # Test addProducToWishlist()
+        request = self.factory.post('addFavourite/<product_id>', {'wishlist': []})
+        request.user = AnonymousUser()
         middleware = SessionMiddleware(request)
         middleware.process_request(request)
-        request.user = AnonymousUser()
-        Profile.addProductToWishlist(request, '1')
-        self.assertEqual(request.session['wishlist'], ['1'])
+        # Tests for Anonymous User
+        self.assertFalse(request.user.is_authenticated)
+        # Add product to empty wishlist
+        Profile.addProductToWishlist(request, 1)
+        self.assertEqual(set(request.session['wishlist']), set([1]))
+        # Add product to wishlist with 1 item
+        Profile.addProductToWishlist(request, 3)
+        self.assertEqual(set(request.session['wishlist']), set([1, 3]))
+        # Add product to wishlist with 2 items
+        Profile.addProductToWishlist(request, 5)
+        self.assertEqual(set(request.session['wishlist']), set([1, 3, 5]))
+        # Add product already in wishlist
+        Profile.addProductToWishlist(request, 5)
+        self.assertEqual(set(request.session['wishlist']), set([1, 3, 5]))
+        # Tests for Authenticated User
+        request = self.factory.post('addFavourite/<product_id>', {'wishlist': []})
+        request.user = self.user1
+        middleware = SessionMiddleware(request)
+        middleware.process_request(request)
+        self.assertTrue(request.user.is_authenticated)
+        profile = Profile.objects.get(user = request.user)
+        self.assertEqual(set(profile.wishlist.all()), set([]))
+        # Add product to empty wishlist
+        Profile.addProductToWishlist(request, 1)
+        self.assertEqual(set(profile.wishlist.all()), set([self.product_1]))
+        # Add product to wishlist with 1 item
+        Profile.addProductToWishlist(request, 3)
+        product_1 = Product.objects.get(id = 1)
+        product_3 = Product.objects.get(id = 3)
+        self.assertEqual(set(profile.wishlist.all()), set([product_1, product_3]))
+        # Add product to wishlist with 2 items
+        Profile.addProductToWishlist(request, 5)
+        self.assertEqual(set(profile.wishlist.all()), set([product_1, product_3, self.product_5]))
+        # Add product already in wishlist
+        Profile.addProductToWishlist(request, 1)
+        self.assertEqual(set(profile.wishlist.all()), set([product_1, product_3, self.product_5]))
 
-        #add to autheniticated user's wishlist
+
+    def test_getWishlistProducts(self):
+        request = self.factory.get('/showWishList', {'wishlist': []})
+        request.user = AnonymousUser()
+        middleware = SessionMiddleware(request)
+        middleware.process_request(request)
+        request.session['wishlist'] = [1, 3, 5]
+        # Tests for Anonymous User
+        self.assertFalse(request.user.is_authenticated)
+        products = Profile.getWishlistProducts(request)
+        self.assertEqual(set(products), set([self.product_1, self.product_3, self.product_5 ]))
+        request.session['wishlist'] = []
+        # Tests for Anonymous User
+        self.assertFalse(request.user.is_authenticated)
+        products = Profile.getWishlistProducts(request)
+        self.assertEqual(set(products), set([]))
+
+        # Tests for Authenticated user
+        request = self.factory.get('/showWishList', {'wishlist': []})
+        request.user = self.user1
+        middleware = SessionMiddleware(request)
+        middleware.process_request(request)
+        self.assertTrue(request.user.is_authenticated)
+        profile = Profile.objects.get(user = request.user)
+        self.assertEqual(set(profile.wishlist.all()), set([]))
+        products = Profile.getWishlistProducts(request)
+        self.assertEqual(set(products), set([]))
+        Profile.addProductToWishlist(request, 5 )
+        products = Profile.getWishlistProducts(request)
+        self.assertEqual(set(products), set([self.product_5]))
+
+    def test_removeProductFromWishlist(self):
+        # Tests for anonymous user
+        request = self.factory.post('removeWishListItem/<product_id>', {'wishlist': []})
+        request.user = AnonymousUser()
+        middleware = SessionMiddleware(request)
+        middleware.process_request(request)
+        self.assertFalse(request.user.is_authenticated)
+        Profile.addProductToWishlist(request, 1 )
+        Profile.addProductToWishlist(request, 3 )
+        Profile.addProductToWishlist(request, 5 )
+        Profile.removeProductFromWishlist(request, 5 )
+        self.assertEqual(set(Profile.getWishlistProducts(request)), set([self.product_1, self.product_3]))
+        Profile.removeProductFromWishlist(request, 3 )
+        self.assertEqual(set(Profile.getWishlistProducts(request)), set([self.product_1]))
+
+        # tests for authenticated user
+        request = self.factory.post('removeWishListItem/<product_id>', {'wishlist': []})
+        request.user = self.user1
+        middleware = SessionMiddleware(request)
+        middleware.process_request(request)
+        self.assertTrue(request.user.is_authenticated)
+        profile = Profile.objects.get(user = request.user)
+        profile.wishlist.add(self.product_1)
+        profile.wishlist.add(self.product_3)
+        profile.wishlist.add(self.product_5)
+        Profile.removeProductFromWishlist(request, 3)
+        self.assertEqual(set(profile.wishlist.all()), set([self.product_1, self.product_5]))
+
+    def test_deleteWishList(self):
+        # test for anonymous user
+        request = self.factory.post('deleteWishList', {'wishlist': []})
+        request.user = AnonymousUser()
+        middleware = SessionMiddleware(request)
+        middleware.process_request(request)
+        self.assertFalse(request.user.is_authenticated)
+        Profile.addProductToWishlist(request, 1 )
+        Profile.addProductToWishlist(request, 3 )
+        Profile.addProductToWishlist(request, 5 )
+        Profile.deleteWishList(request)
+        self.assertEqual(set(request.session['wishlist']), set([]))
+
+        # test for authenticated user
+        request = self.factory.post('deleteWishList', {'wishlist': []})
+        request.user = self.user1
+        middleware = SessionMiddleware(request)
+        middleware.process_request(request)
+        self.assertTrue(request.user.is_authenticated)
+        profile = Profile.objects.get(user = request.user)
+        Profile.deleteWishList(request)
+        self.assertEqual(set(profile.wishlist.all()), set([]))
